7f1cceede285bdf887f824ea5588240283f4c8b0
bakarico <yawaza.rico@gmail.com>
2015-03-14 21:21:06 +0800

    use UITableView to list todos

--- todoFly/ViewController.swift
+++ todoFly/ViewController.swift
@@ -8,34 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
-    @IBOutlet weak var todoView: UIView!
+    @IBOutlet weak var todoTableView: UITableView!
+    
+    var todoList: Array<Todo> = []
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection: Int) -> Int {
+        return self.todoList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: UITableViewCell = self.todoTableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        cell.textLabel?.text = self.todoList[indexPath.row].todoText
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("#\(indexPath.row + 1) cell selected")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         var todo1 = Todo(text: "Test1")
         var todo2 = Todo(text: "Todo2")
         
         todo2.status = todoStatus.done
         
-        var todoList: Array<Todo> = []
+        self.todoList.append(todo1)
+        self.todoList.append(todo2)
         
-        todoList.append(todo1)
-        todoList.append(todo2)
-        
-        let width = self.todoView.frame.size.width
-        
-        var count = 1
-        for todo in todoList {
-            let label = UILabel(frame: CGRectMake(20, CGFloat(count * 40), width, 20))
-            label.text = "\(todo.todoText)(\(todo.status.getStatus()))"
-            //println(label.text)
-            self.todoView.addSubview(label)
-            count++
-        }
+        self.todoTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
     }
 
     override func didReceiveMemoryWarning() {
